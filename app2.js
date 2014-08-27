636ae4ca288c4261ef62a74f55b5248eecc72e30
Sean Thomas Burke <hawaiianchimp@gmail.com>
2014-08-26 17:51:48 -0700

    trying to get abt.com to work

--- app2.js
+++ app2.js
@@ -3,18 +3,42 @@ var dotenv = require('dotenv'),
 	handlebars = require("express3-handlebars"),
 	Crawler = require("crawler").Crawler,
 	robots = require('robots'),
+	colors = require('colors'),
+	icon = require('log-symbols'),
 	// async = require('async'),
 	rparser = new robots.RobotsParser(),
 	sitemapper = new require('sitemapper'),
 	sitemaps = [],
 	sites = [],
-	rtimeout = 200,
+	children = [],
 	cp = require('child_process');
 
-var NUMBER_OF_CRAWLERS = 20;
+//set theme colors
+colors.setTheme({
+	silly: 'rainbow',
+	input: 'grey',
+	verbose: 'cyan',
+	prompt: 'grey',
+	info: 'blue',
+	success: 'green',
+	data: 'grey',
+	help: 'cyan',
+	warn: 'yellow',
+	debug: 'blue',
+	error: 'red'
+});
 
+//number of crawlers to use. More crawlers uses more resources and 
+var NUMBER_OF_CRAWLERS = 3;
+
+//for calculating total time
 var start = new Date();
 var end = new Date();
+
+
+//TODO
+//if we want to see results from a broser
+
 dotenv.load();
 //express
 var app = express();
@@ -23,37 +47,57 @@ app.set('view engine', 'html');
 app.set('port', process.env.PORT);
 
 
-//robots.txt
-var domain = 'www.abt.com';
-var robots_url =  'http://' + domain +'/robots.txt';
 
-console.log("setting sites")
+
+//robots.txt of the site
+var domain = 'www.abt.com';
+var robots_url =  'http://'+domain+'/robots.txt';
+
 rparser.setUrl(robots_url, function(parser, success){
-	console.log("setting URL:", robots_url)
+	console.log(icon.info, "Robots.txt URL:", robots_url)
 	if(success){
-		console.log(success);
 		parser.getSitemaps(function(sitemaps)
 		{
-			console.log("getting sitemaps:", sitemaps);
-			sitemaps = sitemaps;
-			console.log(sitemaps[0]);
+			console.log("Sitemap URL(s):", sitemaps);
+			//get only the first sitemap if there is a list
 			sitemapper.getSites(sitemaps[0], function(err, sites){
 				sites = sites.filter(function(e){
+					//Regex to filter only valid urls; return true to crawl
 					return (e.match(/http(s)?:\/\/((www|.+)\.)?abt\.com\/product/)) ? true: false;
 				});
+				//crawler information
+				console.log("# of sitemap sites: ", ("" + sites.length).info);
+				console.log("# of crawlers     : ", ("" + NUMBER_OF_CRAWLERS).info);
+				var incr = Math.round(sites.length/NUMBER_OF_CRAWLERS);
+				console.log("sites per crawler : ", ("" + incr).info);
 
-				console.log("# of sitemap sites: ", sites.length);
-				console.log(sites);
-				var incr = (sites.length/NUMBER_OF_CRAWLERS);
-				console.log(incr)
-				for(var i=0; i<sites.length;i+=incr){
-					console.log(i," - ",i+incr-1);
-					var spider = cp.fork(__dirname+'/spider.js', sites.slice(i,i+incr-1));
+				//spawn child crawlers
+				for(var i=0; i<NUMBER_OF_CRAWLERS;i++){
+					console.log('\r\n\r\nSpawning crawler for sites:', ((i*incr+1) +"-" + (i*incr+incr)).info);
+
+					//create a thread for the each child with the 'spider.js' code
+					children[i] = cp.fork(__dirname+'/spider.js', sites.slice(i*incr,i*incr+incr));
+   					console.log('Crawler '+(i+1)+' spawned, pid: '+("" + children[i].pid).help);
+
+   					//calculate time on exit
+					children[i].on('exit', function (code) {
+						//when crawler finishes calculate time and log to console.
+						var end = new Date;
+						console.log((""+end).input);
+						console.log(("Total elapsed time: "+(end.getTime() - start.getTime())/1000 + "s").input);
+						children[i] = undefined;
+					});
+
 				}
-				//var spider = spawn('node', ['spider.js', "http://www.seantburke.com/"]);
 			})
 		});
-		console.log(rtimeout);
 	}
+});
+
+//exit on process termination signal
+process.on("SIGTERM", function() {
+   console.log("Parent SIGTERM detected");
+   // exit cleanly
+   process.exit();
 });
 
